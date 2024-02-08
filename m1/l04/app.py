815fbef1cc67efb101f2181ba012d2508130ed62
Drew Valentin <jvestudios2047@gmail.com>
2024-02-08 14:27:37 -0400

    update 15

--- m1/l04/app.py
+++ m1/l04/app.py
@@ -8,5 +8,11 @@ for n in range(1,4):
 print("While #1")
 n = 0
 while (n < 4):
-    n = n + 1
     print(n)
+    n = n + 1
+
+opcion = 1
+while (opcion !=0):
+    opcion = int(input("0  Terminar - 1    Continuar: "))
+
+print("Fin")
