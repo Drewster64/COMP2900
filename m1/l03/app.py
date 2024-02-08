7741e12439c008279cf28dc3a7d61767db96638d
Drew Valentin <jvestudios2047@gmail.com>
2024-02-08 14:08:01 -0400

    update 11

--- m1/l03/app.py
+++ m1/l03/app.py
@@ -1,7 +1,17 @@
-payRate = float(input("Pago por Hora: "))
+payRate = float(input("Pago por Hora: ")) 
 hourRate = float(input("Horas trabajadas: "))
 
 overtime = 0
 
 if (hourRate > 40):
-    
+    overtimeHours = hourRate - 40
+    totalPay = (40 * payRate) + (overtimeHours * (payRate * 2))
+    print("El empleado trabajo overtime.")
+    print(f"El sueldo a pagar al empleado es {totalPay}")
+
+elif (hourRate > 40):
+    print("El empleado trabajo overtime.")
+
+else:
+    totalPay = (hourRate * payRate)
+    print(f"El sueldo a pagar al empleado es {totalPay}")
