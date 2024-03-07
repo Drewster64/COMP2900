4add2caabfe02fba9e59557d0488a5980596b02f
Drew Valentin <jvestudios2047@gmail.com>
2024-03-07 17:34:48 -0400

    update 38

---
+++ m2/l5Arrays/ej9/app.py
@@ -0,0 +1,15 @@
+"""
+9. Combinar dos arreglos en uno solo:
+●	Crea dos arreglos con números.
+●	Combina los dos arreglos en uno solo.
+●	Imprime el arreglo combinado.
+"""
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+numeros2 = [355, 37, 2895, 916, 20015]
+
+print("Lista de elementos del arreglo 1:", numeros)
+print("Lista de elementos del arreglo 2:", numeros2)
+
+numeros3 = numeros + numeros2
+
+print("Arreglo combinado:", numeros3)
