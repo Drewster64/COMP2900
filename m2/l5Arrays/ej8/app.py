9c852c79082d45ca64bf570794d0eb4e737a06f4
Drew Valentin <jvestudios2047@gmail.com>
2024-03-07 17:30:35 -0400

    update 37

---
+++ m2/l5Arrays/ej8/app.py
@@ -0,0 +1,11 @@
+"""
+. Ordenar los elementos de un arreglo de menor a mayor:
+●	Crea un arreglo con números.
+●	Ordena los elementos del arreglo de menor a mayor.
+●	Imprime el arreglo ordenado.
+"""
+
+numeros = [355, 37, 2895, 916, 20015]
+print("Lista de elementos:", numeros)
+numeros.sort() #Funcion sort permite organizarlas de menor a mayor
+print("Lista ordenada:", numeros)
