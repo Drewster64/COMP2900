e59bcd5a9530fb6cd1ae22212452f1bd4e0213b4
Drew Valentin <jvestudios2047@gmail.com>
2024-03-11 19:54:10 -0400

    update 42

--- m2/l5/ej6/app.py
+++ m2/l5/ej6/app.py
@@ -1,8 +1,16 @@
+"""
+6. Invertir una cadena de texto:
+●	Función: invertir_cadena(cadena)
+●	Parámetro: cadena (texto a invertir)
+●	Devuelve: La cadena de texto invertida
+"""
+
 def invertir_cadena(cadena):
-    return ''.join(reversed(cadena))
-#reversed invierte el texto y lo devuelve
-#join junta el exto invertido en una cadena
+    invertida = ""
+    for caracter in cadena:
+        invertida = caracter + invertida
+    return invertida
 
 texto = input("Ingrese una cadena de texto: ")
-texto_invertido = invertir_cadena(texto)#llama funcion con texto para la cadena
-print("El texto invertida es:", texto_invertido)
+texto_invertido = invertir_cadena(texto)
+print("El texto invertido es:", texto_invertido)
