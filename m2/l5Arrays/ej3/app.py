6054c116954e31112699df3ef1273cf45abfee52
Drew Valentin <jvestudios2047@gmail.com>
2024-03-12 13:47:27 -0400

    update 46

--- m2/l5Arrays/ej3/app.py
+++ m2/l5Arrays/ej3/app.py
@@ -7,7 +7,15 @@ Invertir el orden de los elementos de un arreglo:
 
 numeros = [355, 37, 2895, 916, 20015]
 
-numeros_invertidos = list(reversed(numeros)) #metodo reversed permite invertir los valores
+numeros_invertidos = []
+
+longitud = 0
+for _ in numeros: # Obtiene la longitud de la lista
+    longitud += 1
+
+for i in range(longitud - 1, -1, -1):
+    numeros_invertidos.append(numeros[i]) #los -1 permiten reorganizar los valores en orden
 
 print("Lista de numeros original:", numeros)
 print("Lista de numeros con el orden invertido:", numeros_invertidos)
+
