228d42f49e9f6c8e6aa8fca234bf334ece6d8dc6
Drew Valentin <jvestudios2047@gmail.com>
2024-03-12 14:21:22 -0400

    update 48

--- m2/l5Arrays/ej5/app.py
+++ m2/l5Arrays/ej5/app.py
@@ -9,9 +9,42 @@ numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 print("Lista de elementos:", numeros)
 deleteNum = int(input("Ingrese el elemento que desea eliminar del arreglo: "))
 
+posicion = -1
+counter = 0
+for numero in numeros:
+    if numero == deleteNum:
+        posicion = counter
+        break
+    counter += 1
+
+if posicion != -1: # Verifica si el elemento a eliminar esta en la lista
+    nuevos_numeros = []
+    counter = 0
+    for numero in numeros:
+        if counter != posicion:
+            nuevos_numeros.append(numero)
+        counter += 1
+    numeros = nuevos_numeros
+
+    print("El elemento", deleteNum, "fue eliminado del arreglo.")
+    print("Lista de elementos actualizada:", numeros)
+else:
+    print("El elemento", deleteNum, "no se encuentra en la lista de elementos")
+
+
+
+"""
+******************************Con funciones especiales**********************************
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+print("Lista de elementos:", numeros)
+deleteNum = int(input("Ingrese el elemento que desea eliminar del arreglo: "))
+
 if deleteNum in numeros:
     numeros.remove(deleteNum) #funcion remove permite eliminar el numero escogido por el usuario del arreglo
     print("El elemento", deleteNum, "fue eliminado del arreglo.")
     print("Lista de elementos actualizada:", numeros)
 else:
     print("El elemento", deleteNum, "no se encuentra en la lista de elementos")
+"""
--- m2/l5Arrays/ej6/app.py
+++ m2/l5Arrays/ej6/app.py
@@ -6,13 +6,17 @@
 """
 numeros = [2, 22, 59, 33, 33, 66, 156, 33]
 
-# Pedimos al usuario que ingrese el elemento que desea contar
 print("Arreglo de Numeros:", numeros)
 countNum = int(input("Ingrese el elemento que desea contar en el arreglo de numeros: "))
 
-veces = numeros.count(countNum) #Funcion count permite contar el elemento en la cadena del arreglo
-if (veces == 1): 
-    print("El elemento", countNum, "aparece", veces, "vez en el arreglo de numeros.")
+counter = 0 
+
+for numero in numeros:
+    if numero == countNum:
+        counter+= 1
+
+if counter == 1:
+    print("El elemento", countNum, "aparece una vez en el arreglo de numeros.")
 else:
-    print("El elemento", countNum, "aparece", veces, "veces en el arreglo de numeros.")
+    print("El elemento", countNum, "aparece", counter, "veces en el arreglo de numeros.")
 
