77ebc430d5240b76cb6fd8f7aff996734bd8f3c9
Drew Valentin <jvestudios2047@gmail.com>
2024-03-13 21:08:33 -0400

    update 50

--- m2/l5Arrays/ej8/app.py
+++ m2/l5Arrays/ej8/app.py
@@ -4,8 +4,29 @@
 ●	Ordena los elementos del arreglo de menor a mayor.
 ●	Imprime el arreglo ordenado.
 """
+numeros = [355, 37, 2895, 916, 20015]
+print("Lista de elementos:", numeros)
 
+n = 0 
+for b in numeros:
+    n += 1
+
+i = 0
+while i < n:
+    O = i + 1 #evita que los elementos se comparen con ellos mismos
+    while O < n:
+        if numeros[i] > numeros[O]: #si los numeros de i son mayor a O, se intercambian
+            numeros[i], numeros[O] = numeros[O], numeros[i]
+        O += 1
+    i += 1
+
+print("Lista ordenada de menor a mayor:", numeros)
+
+
+"""
 numeros = [355, 37, 2895, 916, 20015]
 print("Lista de elementos:", numeros)
 numeros.sort() #Funcion sort permite organizarlas de menor a mayor
 print("Lista ordenada:", numeros)
+"""
+
