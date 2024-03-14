46ca8a5ba380a8904dd02da83fa9bf88ca1882d9
Drew Valentin <jvestudios2047@gmail.com>
2024-03-13 20:56:04 -0400

    update 49

--- m2/l5Arrays/ej7/app.py
+++ m2/l5Arrays/ej7/app.py
@@ -4,6 +4,36 @@
 ●	Calcula el promedio de los elementos del arreglo.
 ●	Imprime el promedio.
 """
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+print("Lista de elementos:", numeros)
+deleteNum = int(input("Ingrese el elemento que desea eliminar del arreglo: "))
+
+posicion = -1 #Almacena el elemento escogido
+
+counter = 0
+for numero in numeros:
+    if numero == deleteNum:
+        posicion = counter 
+    counter  += 1
+
+if posicion != -1:
+    nuevos_numeros = []
+    counter = 0
+    for numero in numeros:
+        if counter != posicion:
+            nuevos_numeros.append(numero)
+        counter += 1
+    numeros = nuevos_numeros
+
+    print("El elemento", deleteNum, "fue eliminado del arreglo.")
+    print("Lista de elementos actualizada:", numeros)
+else:
+    print("El elemento", deleteNum, "no se encuentra en la lista de elementos")
+
+"""
+******************************Con funciones especiales**********************************
+
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 promedio = sum(numeros) / len(numeros)
@@ -11,3 +41,4 @@ promedio = sum(numeros) / len(numeros)
 
 print("Lista de elementos:", numeros)
 print("El promedio de los elementos del arreglo es:", promedio)
+"""
